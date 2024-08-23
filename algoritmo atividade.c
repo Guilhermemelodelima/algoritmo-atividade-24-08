e9f22d390a9a83d754deb8ba52958131da23a361
Guilhermemelodelima <guilhermemellodellima@gmail.com>
2024-08-23 18:12:33 -0300

    Add files via upload

---
+++ algoritmo atividade.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int tempoBom, amigoDisponivel, dinheiroSuficiente;
+    
+    printf("O tempo esta bom (1 para Sim, 0 para Nao)? ");
+    scanf("%d", &tempoBom);
+    
+    printf("O seu amigo esta livre (1 para sim, 0 para nao)? ");
+    scanf("%d", &amigoDisponivel);
+    
+    printf("Voce possui dinheiro suficiente (1 para sim, 0 para não)? ");
+    scanf("%d", &dinheiroSuficiente);
+
+    if (tempoBom && amigoDisponivel && dinheiroSuficiente) {
+        printf("Você vai para o passeio!\n");
+    } else if (!tempoBom || !amigoDisponivel || !dinheiroSuficiente) {
+        printf("Você não vai para o passeio.\n");
+    } 
+    
+    return 0;
+}
+
